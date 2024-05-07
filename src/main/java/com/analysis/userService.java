0e53985d62f11e19bd03548f522c3d219c216bfe
Lanna-27 <lalacast2000@gmail.com>
2024-05-07 01:22:33 -0500

    Fix userService

--- src/main/java/com/analysis/userService.java
+++ src/main/java/com/analysis/userService.java
@@ -4,17 +4,9 @@ import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
 import jakarta.jws.WebService;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-//POJO
-
-import org.json.JSONObject;
-
-//TODO change methods to match with the GraphQL service
 @WebService(serviceName = "UserWS")
 public class userService {
-    GraphQLSOAPService graphQLSOAPService = new GraphQLSOAPService();
+    GraphQLSOAPUserService graphQLSOAPService = new GraphQLSOAPUserService();
     //     
     @WebMethod(operationName = "StatsFollowers")
     public String getStatsFollowers(){
