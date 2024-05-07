be08df1687d95e80dda9ee243843d998a69be024
Lanna-27 <lalacast2000@gmail.com>
2024-05-07 01:21:25 -0500

    Changing Name of GraphSQL Service

--- src/main/java/com/analysis/GraphQLSOAPUserService.java
+++ src/main/java/com/analysis/GraphQLSOAPUserService.java
@@ -1,106 +1,4 @@
 package com.analysis;
-// import java.io.*;
-// import java.net.HttpURLConnection;
-// import java.net.URL;
-// import java.util.List;
-// import org.json.JSONObject;
-
-// public class GraphQLSOAPService {
-
-//     public String invokeGraphQLService() {
-//         String responseString = "";
-//         try {
-//             // Build the GraphQL request
-//             String graphqlQuery = "query{  users{    uid  }}";
-//             String graphqlRequest = "{\"query\": \"" + graphqlQuery + "\"}";
-
-//             // Set up the connection to the GraphQL server
-//             URL url = new URL("http://localhost:80/graphql");
-//             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//             connection.setRequestMethod("POST");
-//             connection.setRequestProperty("Content-Type", "application/json");
-//             connection.setDoOutput(true);
-
-//             // Send the GraphQL request
-//             OutputStream outputStream = connection.getOutputStream();
-//             outputStream.write(graphqlRequest.getBytes());
-//             outputStream.flush();
-
-//             // Read the response from the GraphQL server
-//             if (connection.getResponseCode() != 200) {
-//                 throw new RuntimeException("Failed : HTTP error code : " + connection.getResponseCode());
-//             }
-
-//             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((connection.getInputStream())));
-
-//             String output;
-//             System.out.println("Output from Server .... \n");
-//             while ((output = bufferedReader.readLine()) != null) {
-//                 responseString += output;
-//             }
-
-//             connection.disconnect();
-
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//         }
-
-
-//         return responseString;
-//     }
-
-//     private int getUserFollowerCount(String uid) throws IOException {
-//         // Build the GraphQL request to get the followers count for the user
-//         String followersQuery = "query{ followers(id: \"" + uid + "\"){ followers } }";
-//         String graphqlRequest = "{\"query\": \"" + followersQuery + "\"}";
-
-//         // Set up the connection to the GraphQL server
-//         URL url = new URL("http://localhost:80/graphql");
-//         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//         connection.setRequestMethod("POST");
-//         connection.setRequestProperty("Content-Type", "application/json");
-//         connection.setDoOutput(true);
-
-//         // Send the GraphQL request to get the followers for the user
-//         OutputStream outputStream = connection.getOutputStream();
-//         outputStream.write(graphqlRequest.getBytes());
-//         outputStream.flush();
-
-//         // Read the response from the GraphQL server
-//         if (connection.getResponseCode() != 200) {
-//             throw new RuntimeException("Failed : HTTP error code : " + connection.getResponseCode());
-//         }
-
-//         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((connection.getInputStream())));
-//         StringBuilder followersResponseBuilder = new StringBuilder();
-//         String output;
-//         while ((output = bufferedReader.readLine()) != null) {
-//             followersResponseBuilder.append(output);
-//         }
-//         bufferedReader.close();
-
-//         // Parse the response to extract the follower count
-//         // Implement parsing logic here
-//         // int followerCount = parseFollowerCount(followersResponseBuilder.toString());
-//         // return followerCount;
-
-//         return 0; // Placeholder return value
-//     }
-
-//     private double calculateAverageFollowerCount(List<Integer> followerCounts) {
-//         double totalFollowerCount = 0;
-//         for (int count : followerCounts) {
-//             totalFollowerCount += count;
-//         }
-//         return totalFollowerCount / followerCounts.size();
-//     }
-
-//     // public static void main(String[] args) {
-//     //     GraphQLSOAPService graphQLSOAPService = new GraphQLSOAPService();
-//     //     String response = graphQLSOAPService.invokeGraphQLService();
-//     //     System.out.println(response);
-//     // }
-// }
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -108,7 +6,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class GraphQLSOAPService {
+public class GraphQLSOAPUserService {
 
     public String getAverageFollows() {
         try {
